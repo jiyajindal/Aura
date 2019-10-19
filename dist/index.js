b087931cdb9b21d24c9c3a4dfde3ca2049687f67
blistersun <dreisotalbo@gmail.com>
2019-10-19 18:31:33 +1100

    test

--- dist/index.js
+++ dist/index.js
@@ -60,7 +60,7 @@ let provider = new H.datalens.RawDataProvider({
                 "type": "Feature",
                 "geometry": {
                     "type": "Point",
-                    "coordinates": [Number(row[3), Number(row[2])]
+                    "coordinates": [Number(row[3]), Number(row[2])]
                 },
                 "properties": {
                     "pollutant": row[20]
